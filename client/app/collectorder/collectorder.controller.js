4445fbef591c6da3c778ca72402d8590c6581462
Sithu <sithukyaw007@gmail.com>
2015-04-07 05:26:40 +0800

    implemented collection feature

--- client/app/collectorder/collectorder.controller.js
+++ client/app/collectorder/collectorder.controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('goffeeAppApp')
-.controller('CollectorderCtrl', function ($scope) {
+.controller('CollectorderCtrl', function ($scope, $timeout, $rootScope) {
 	$scope.message = 'Hello';
+
+	$scope.captureImg = function() {
+		debugger;
+		$scope.showCircular = true;
+		$scope.myClick();
+	};
+
+	$scope.myClick = function() {
+		$timeout(
+			function() {
+				$scope.showSuccessMsg = true;
+				$scope.showCircular = false;
+				$rootScope.successCollection = true;
+			}, 5000);
+	};
 });
+
